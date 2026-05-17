eaf6d74858a81b087e06676bb06897d67a3eae97
compmateriaischns-sketch <comp.materiais.chns@gmail.com>
2026-05-17 11:15:21 -0300

    Remove e ignora pasta BEECROWD-P02

--- BEECROWD-P02/1172.py
+++
@@ -1,5 +0,0 @@
-v = [int(input("Digite os elementos do vertor: ")) for i in range(10)]
-for i in range(10):
-    if v[i] <= 0:
-        v[i] = 1
-    print(f"X[{i}] = {v[i]}")
--- BEECROWD-P02/1175.py
+++
@@ -1,9 +0,0 @@
-N = [int(input()) for i in range(7)]
-b = len(N)
-print(N)
-for i in range(b//2):
-    a = N[i]
-    N[i] = N[b-i-1]
-    N[b-i-1] = a
-for i in range(b):
-    print(f'N[{i}] = {N[i]}')
